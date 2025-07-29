1942ff5b44db9f164d5b24dcc1b84320584348fd
PhanHieu244 <phanhieu2443@gmail.com>
2025-07-29 23:16:52 +0700

    init

---
+++ Assets/Scripts/CompleteProject/IAP.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace CompleteProject
+{
+	public class IAP : MonoBehaviour
+	{
+
+		public static string kProductIDRemovesAdvertisment = "com.gameinlife.merged.remove_advertisments";
+
+		public static string kProductIDDoubleCoins = "com.gameinlife.merged.double_coins";
+
+		public static string kProductID1000Coins = "com.gameinlife.merged.1000_coins";
+
+		public static string kProductID4000Coins = "com.gameinlife.merged.4000_coins";
+
+		public static string kProductID20000Coins = "com.gameinlife.merged.20000_coins";
+
+		public static string kProductIDoubleCoinsInAndroid = "com.gameinlife.merged_double.coins";
+
+		public static string kProductI1000CoinsInAndroid = "com.gameinlife.merged_1000.coin";
+
+		public static string kProductI4000CoinsInAndroid = "com.gameinlife.merged_4000.coins";
+
+		public static string kProductI20000CoinsInAndroid = "com.gameinlife.merged_20000.coins";
+
+		private static string kProductNameAppleSubscription = "com.unity3d.subscription.new";
+
+		private static string kProductNameGooglePlaySubscription = "com.unity3d.subscription.original";
+
+		private static Action<bool> __f__am_cacheD;
+	}
+}
